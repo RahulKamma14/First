978de016c573113c1a20e7af176af8f9712a7f22
RahulKamma <rahul.kamma14@gmail.com>
2023-08-16 22:31:06 -0400

    Day3 - getting single resource

--- src/main/java/com/example/project/first/topic/TopicController.java
+++ src/main/java/com/example/project/first/topic/TopicController.java
@@ -1,19 +1,24 @@
 package com.example.project.first.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+
 import java.util.List;
 
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
     @RequestMapping("/topics")
-    public List<Topic> sayTopic() {
-        return Arrays.asList(
-            new Topic("spring", "Spring Framework", "Spring Framework Description"),
-            new Topic("java", "Core Java", "Core Java Description"),
-            new Topic("javascript", "JavaScript", "JavaScript Description"));
+    public List<Topic> getAllTopics() {
+        return topicService.getAllTopics();
+    }
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
     }
 }
